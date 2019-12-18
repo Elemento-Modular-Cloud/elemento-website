8dc8aeb8dc30091a3fc44f7647e8e806b1cb299c
Gabriele <sucre.91@hotmail.it>
2019-12-18 23:32:11 +0100

    tests

---
+++ js/fade-elements-test.js
@@ -0,0 +1,20 @@
+var lastScrollTop = 0;
+$(window).on("scroll", function(){
+  var scrollTop = $(this).scrollTop();
+  $('.scrollDiv').each(function(){
+    var el = $(this);
+    var offsetTop = el.offset().top;
+    if(scrollTop < offsetTop){    
+      el.removeClass("fade-out");
+    }else{
+      el.addClass("fade-out");
+    }
+
+    var offsetTop2 = el.offset().top * 1.5;
+    if(scrollTop < offsetTop2){  
+      el.addClass("fade-in");
+    }else{
+      el.removeClass("fade-in");
+    }
+  });
+});
