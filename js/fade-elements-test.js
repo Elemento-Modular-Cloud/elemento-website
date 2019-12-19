3077d25bcb8430fa9a12c3b04e57787b012fed54
Gabriele <sucre.91@hotmail.it>
2019-12-19 10:07:55 +0100

    Tests

--- js/fade-elements-test.js
+++ js/fade-elements-test.js
@@ -3,14 +3,14 @@ $(window).on("scroll", function(){
   var scrollTop = $(this).scrollTop();
   $('.scrollDiv').each(function(){
     var el = $(this);
-    var offsetTop = el.offset().top;
+    var offsetTop = el.offset().top * 1.08;
     if(scrollTop < offsetTop){    
       el.removeClass("fade-out");
     }else{
       el.addClass("fade-out");
     }
 
-    var offsetTop2 = el.offset().top * 1.5;
+    var offsetTop2 = el.offset().top;
     if(scrollTop < offsetTop2){  
       el.addClass("fade-in");
     }else{
