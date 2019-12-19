5f39bab8ba68473460031f13c648e00c44deb257
Gabriele <sucre.91@hotmail.it>
2019-12-19 12:57:38 +0100

    Improving js and index syntax

--- js/fade-elements.js
+++ js/fade-elements.js
@@ -1,19 +1,15 @@
-var lastScrollTop = 0;
 $(window).on("scroll", function(){
   var scrollTop = $(this).scrollTop();
   $('.scrollDiv').each(function(){
     var el = $(this);
-    var offsetTop = el.offset().top * 1.08;
-    if(scrollTop < offsetTop){    
+    var offsetTop = el.offset().top;
+    if(scrollTop < offsetTop * 1.08){    
       el.removeClass("fade-out");
-    }else{
-      el.addClass("fade-out");
-    }
-
-    var offsetTop2 = el.offset().top * 1.5;
-    if(scrollTop < offsetTop2){  
       el.addClass("fade-in");
-    }else{
+    } else if(scrollTop < offsetTop * 1.5){
+      el.addClass("fade-out");
+    } else {
+      el.addClass("fade-out");
       el.removeClass("fade-in");
     }
   });
