c09cca07010ac2b389d0485f00a3d758f28a22b5
Gabriele <sucre.91@hotmail.it>
2019-12-19 15:50:56 +0100

    Fading out stars on scroll

--- js/fade-border.js
+++ js/fade-border.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
 var myElement = $('.border-icon');
+var stars = $('.stars');
 $(window).on('scroll', function () {
-    var st = $(this).scrollTop()*.7;
+    var st = $(this).scrollTop();
     myElement.css({
-        'opacity': 1 - st / 600
+        'opacity': 1 - st * .7 / 600
     });
+
+    if ($(this).scrollTop() > 0) {
+        stars.fadeOut(1700);
+    } else {
+        stars.fadeIn(1000);
+    }
 });
 });
     
