76b6b599b3ceea2f9ed9e2891a17c62066ecfc23
gabrielefronze <sucre.91@hotmail.it>
2020-01-14 15:15:54 +0100

    Disabling hover when menu is opened

--- js/menu-buttons.js
+++ js/menu-buttons.js
@@ -1,8 +1,9 @@
 function toggleMenu() {
-    document.getElementsByClassName("fullpage_menu")[0].classList.toggle("visible_menu");
+    document.getElementsByClassName("menu")[0].classList.toggle("enable_hover");
     var bars = document.getElementsByClassName("bar");
     for(var i = 0; i < bars.length; i++)
     {
         bars.item(i).classList.toggle("hoverbar");
     }
+    document.getElementsByClassName("fullpage_menu")[0].classList.toggle("visible_menu");
 }
