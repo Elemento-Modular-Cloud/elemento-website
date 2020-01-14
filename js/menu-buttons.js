45dda7d94fa8d300088d9c9bed1e6e0fee6e1bca
gabrielefronze <sucre.91@hotmail.it>
2020-01-14 12:47:14 +0100

    Commuting three bars menu into close button

--- js/menu-buttons.js
+++ js/menu-buttons.js
@@ -1,3 +1,8 @@
 function toggleMenu() {
-    document.getElementsByClassName("fullpage_menu")[0].classList.toggle("visible");
+    document.getElementsByClassName("fullpage_menu")[0].classList.toggle("visible_menu");
+    var bars = document.getElementsByClassName("bar");
+    for(var i = 0; i < bars.length; i++)
+    {
+        bars.item(i).classList.toggle("hoverbar");
+    }
 }
