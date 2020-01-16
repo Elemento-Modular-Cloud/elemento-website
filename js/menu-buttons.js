ff7820b99337841db6508e8fe3289d78d3c3b264
gabrielefronze <sucre.91@hotmail.it>
2020-01-16 10:24:22 +0100

    Disabling menu links to same page programmatically

--- js/menu-buttons.js
+++ js/menu-buttons.js
@@ -4,6 +4,14 @@ function toggleMenu() {
     var bars = document.getElementsByClassName("bar");
     for(var i = 0; i < bars.length; i++)
     {
-        bars.item(i).classList.toggle("hoverbar");
+        bars[i].classList.toggle("hoverbar");
+    }
+
+    var thispageurl = window.location.href;
+    var links = document.getElementsByTagName("a");
+    for(var i = 0; i < links.length; i++)
+    {
+        if (links[i].href === thispageurl)
+            links[i].classList.toggle("disabled_link");
     }
 }
