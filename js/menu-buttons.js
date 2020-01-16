06d796f61b0106e0fad7c06ee2c77a1f2e55193a
Gabriele <sucre.91@hotmail.it>
2020-01-16 12:17:42 +0100

    Multiple improvements

--- js/menu-buttons.js
+++ js/menu-buttons.js
@@ -1,9 +1,22 @@
+var call = 0;
+
 function toggleMenu() {
+    call++;
     document.getElementsByClassName("fullpage_menu")[0].classList.toggle("visible_menu");
     document.getElementsByClassName("menu")[0].classList.toggle("enable_hover");
     var bars = document.getElementsByClassName("bar");
     for(var i = 0; i < bars.length; i++)
     {
-        bars.item(i).classList.toggle("hoverbar");
+        bars[i].classList.toggle("hoverbar");
+    }
+
+    if (call === 1){
+        var thispageurl = window.location.href;
+        var links = document.getElementsByTagName("a");
+        for(var i = 0; i < links.length; i++)
+        {
+            if (links[i].href === thispageurl)
+                links[i].classList.add("disabled_link");
+        }
     }
 }
